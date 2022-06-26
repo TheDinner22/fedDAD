f04dfa1757022687d0d1c902701dd832820de38d
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-06-25 21:35:44 -0400

    thing that determines new items 4 sale

---
+++ fedora/lib/this/comparer.ts
@@ -0,0 +1,39 @@
+/**
+ * 
+ * compare json strings to determine if items were added or removed from the freaking thing
+ * 
+ * 
+ * 
+ * 
+ */
+
+import { promiseGet } from "./data";
+import { getElementFromOpenBracketReturnLike } from "./parser";
+
+export function compareJSON(oldItems: getElementFromOpenBracketReturnLike[], newItems: getElementFromOpenBracketReturnLike[]){
+    // convert these lists of obects to lists of htmlElements
+    const oldElements = oldItems.map(item => item.rawElement);
+    const newElements = newItems.map(item => item.rawElement);
+
+    // # of iterations is based on length of the longest list
+    const iterNum = oldElements.length > newElements.length ? oldElements.length : newElements.length;
+    const biggerItemList = oldItems.length > newItems.length ? oldItems : newItems;
+    const biggerElementList = oldElements.length > newElements.length ? oldElements : newElements;
+    const smallerElementList = oldElements.length > newElements.length ? newElements : oldElements;
+    const smallerElementListIsNewOne = oldElements.length > newElements.length ? true : false;
+    for (let i = 0; i < iterNum; i++) {
+        const element = biggerItemList[i].rawElement;
+        const indexOfElementInSmall = smallerElementList.indexOf(element);
+        if( indexOfElementInSmall != -1){
+            // remove element from both element lists
+            biggerElementList.splice(indexOfElementInSmall, 1);
+            smallerElementList.splice(indexOfElementInSmall, 1);
+        }
+    }
+    if(smallerElementListIsNewOne){
+        return smallerElementList;
+    }
+    else{
+        return biggerElementList;
+    }
+};
